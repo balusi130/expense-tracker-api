ff72af6d6047673759473cd03213d1b27006e93b
balusi130 <132842680+balusi130@users.noreply.github.com>
2026-05-23 15:53:48 +0300

    Add JWT security helpers

--- core/security.py
+++ core/security.py
@@ -7,7 +7,7 @@ import os
 
 SECRET_KEY = os.getenv("SECRET_KEY", "changeme")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
